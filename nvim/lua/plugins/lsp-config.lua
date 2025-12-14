68d354b2dbe5d232282191077117ee674709a645
Hassan Sheikha <devazan@Hassans-MacBook-Pro.local>
2025-12-14 02:00:40 +0100

    setup lsp config for lua and ts/js

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    config = function()
+      require('mason').setup()
+    end
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+      require('mason-lspconfig').setup({
+        ensure_installed = {
+          'lua_ls',
+          'ts_ls'
+        }
+      })
+    end
+  },
+  {
+    'neovim/nvim-lspconfig',
+    config = function() 
+      vim.lsp.config('lua_ls', {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' },
+            },
+          },
+        },
+      })
+
+      vim.lsp.config('ts_ls', {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' },
+            },
+          },
+        },
+      })
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 -- Plugin to allow searching for files and live grepping for file contents
 return {
+  {
   'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
@@ -8,5 +9,20 @@ return {
     vim.keymap.set('n', '<C-p>', builtin.find_files, {})
     -- set live grep to leader fg, which in tutorial was space bar. Is set at the top
     vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
-  end 
+  end
+  },
+  {
+  'nvim-telescope/telescope-ui-select.nvim',
+  config = function()
+    require("telescope").setup({
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown {
+          }
+        }
+      }
+    })
+    require("telescope").load_extension("ui-select")
+  end
+  }
 }
